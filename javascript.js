90b64c5aef26e58258d1122afac92c06a581294b
Ben Z <bbatchelder@proton.me>
2026-07-10 21:37:30 -0600

    Make basic functions

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const addition = function (number1, number2) {
+  return number1 + number2;
+}
+
+const subtraction = function (number1, number2) {
+  return number1 - number2;
+}
+
+const multiplication = function (number1, number2) {
+  return number1 * number2;
+}
+
+const division = function (number1, number2) {
+  return number1 / number2;
+}
