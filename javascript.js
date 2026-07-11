60193bd7d64319583310bf9ae6623181577021c0
Ben Z <bbatchelder@proton.me>
2026-07-11 09:19:35 -0600

    make index file

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
+let firstEntry = 0;
+let secondEntry = 0;
+let operator;
+
+const operator_function = function (number1, operator, number2) {
+  if (operator == '+') {
+    return addition(number1, number2);
+  } else if (operator == '-') {
+    return subtraction(number1, number2);
+  } else if (operator == '*') {
+    return multiplication(number1, number2);
+  } else return division(number1, number2);
+}
+
+
 const addition = function (number1, number2) {
   return number1 + number2;
 }
